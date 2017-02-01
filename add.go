f08d3d4fdf7040357175b4f7771a4f6b1a0999e6
Nate Jones <nate@endot.org>
2017-02-01 07:10:48 -0800

    add support for multiline variables

--- add.go
+++ add.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
 type AddCommand struct {
 	Name    string `short:"n" long:"name" description:"Name of environment variable." required:"yes"`
+	File    string `short:"f" long:"file" description:"File with contents of variable"`
 	Exposed string `short:"e" long:"exposed" description:"Name of exposed variable, if different than the name."`
 }
 
 var addCommand AddCommand
 
 func (c *AddCommand) Execute(args []string) error {
+	var err error
+
 	key, err := ReadKey()
 	if err != nil {
 		return errors.Wrap(err, "unable to read key")
@@ -34,9 +39,18 @@ func (c *AddCommand) Execute(args []string) error {
 		exposed = c.Name
 	}
 
-	value, err := PromptForValue()
-	if err != nil {
-		return errors.Wrap(err, "error reading value")
+	var value string
+	if len(c.File) > 0 {
+		data, err := ioutil.ReadFile(c.File)
+		if err != nil {
+			return errors.Wrap(err, "error reading file")
+		}
+		value = strings.TrimSpace(string(data))
+	} else {
+		value, err = PromptForValue()
+		if err != nil {
+			return errors.Wrap(err, "error reading value")
+		}
 	}
 
 	return AddVariable(key, c.Name, exposed, value)
